e885c120253b184614c4436369261975d4c37143
rafaewo <rafaelnogcunha@hotmail.com>
2019-09-16 22:07:18 -0300

    adicionando rotas

---
+++ hackatinder/pages/Home.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+
+    // tem equipe ? path home da equipe : path home alone
+    
+}
+
+export default Home;
---
+++ hackatinder/src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './pages/Home'
+import SearchTeam from './pages/SearchTeam';
+import SearchCrew from './pages/SearchCrew';
+
+function Routes() {
+	return (
+		<Switch>
+			<Route path='/' exact component={Home} />
+			<Route path='/searchTeam' component={SearchTeam} />
+            <Route path='/searchCrew' component={SearchCrew} />
+		</Switch>
+	);
+}
+
+export default Routes;
