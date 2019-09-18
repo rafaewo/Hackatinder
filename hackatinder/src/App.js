239adac04685cb66b3ff4f34aaac6fbd07fe7d4a
rafaewo <rafaelnogcunha@hotmail.com>
2019-09-17 21:51:14 -0300

    ajustando os diretórios e começando o desenvolvimento

--- hackatinder/src/App.js
+++ hackatinder/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import Header from './components/Header'
+// import Header from './components/Header'
 import Routes from './routes';
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
+      {/* <Header /> */}
       <Routes />
     </BrowserRouter>
   );
